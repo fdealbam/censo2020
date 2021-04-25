3da71428c14668b375f722fa43785aa87dfed5d3
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-04-25 18:57:18 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -1,5 +1,6 @@
-
-dash_core_components as dcc
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
